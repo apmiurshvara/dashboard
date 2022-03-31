d5a42a60e967993973f87d522ba62bc3859a4d8f
apurva mishra <ampi.ursh.vara@gmail.com>
2022-03-31 16:05:23 +0530

    initial commit

--- public/dashboard_jsx.js
+++ public/dashboard_jsx.js
@@ -1,8 +1,143 @@
 var App = function App() {
   return React.createElement(
-    "h1",
-    { id: "prop" },
-    "hello from the other side"
+    "div",
+    { id: "main" },
+    React.createElement(
+      "div",
+      { id: "dash" },
+      React.createElement("button", { id: "one" }),
+      React.createElement("button", { id: "two" })
+    ),
+    React.createElement(
+      "div",
+      { id: "page_one" },
+      React.createElement(
+        "div",
+        { id: "form_page" },
+        React.createElement(
+          "h2",
+          null,
+          "FILL CREDENTIALS"
+        ),
+        React.createElement(
+          "form",
+          null,
+          React.createElement(
+            "label",
+            { "for": "input_one", id: "input_one_label" },
+            "FIRST NAME"
+          ),
+          React.createElement("input", { type: "text", id: "input_one" }),
+          React.createElement(
+            "label",
+            { "for": "input_two", id: "input_two_label" },
+            "LAST NAME"
+          ),
+          React.createElement("input", { type: "text", id: "input_two" }),
+          React.createElement(
+            "label",
+            { "for": "input_three", id: "input_three_label" },
+            "E-MAIL"
+          ),
+          React.createElement("input", { type: "text", id: "input_three" }),
+          React.createElement(
+            "label",
+            { "for": "input_four", id: "input_four_label" },
+            "INSTITUTION"
+          ),
+          React.createElement("input", { type: "text", id: "input_four" }),
+          React.createElement("input", { type: "submit", id: "submit" })
+        )
+      )
+    ),
+    React.createElement(
+      "div",
+      { id: "page_two" },
+      React.createElement(
+        "div",
+        { id: "list_one" },
+        React.createElement(
+          "ul",
+          null,
+          React.createElement(
+            "li",
+            null,
+            "apurva"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "mishra"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "apurva.mishra26@yahoo.com"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "delhi iniversity"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { id: "list_two" },
+        React.createElement(
+          "ul",
+          null,
+          React.createElement(
+            "li",
+            null,
+            "ipsum"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "lorem "
+          ),
+          React.createElement(
+            "li",
+            null,
+            "ipsum.lorem@yahoo.com"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "lucknow university"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { id: "list_three" },
+        React.createElement(
+          "ul",
+          null,
+          React.createElement(
+            "li",
+            null,
+            "some"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "thing "
+          ),
+          React.createElement(
+            "li",
+            null,
+            "some.thing@yahoo.com"
+          ),
+          React.createElement(
+            "li",
+            null,
+            "lucknow university"
+          )
+        )
+      )
+    )
   );
 };
 ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
--- public/src/dashboard_jsx.jsx
+++ public/src/dashboard_jsx.jsx
@@ -1,4 +1,63 @@
 const App = function () {
-  return <h1 id="prop">hello from the other side</h1>;
+  return (
+    <div id="main">
+      <div id="dash">
+        <button id="one"></button>
+        <button id="two"></button>
+      </div>
+      <div id="page_one">
+        <div id="form_page">
+          <h2>FILL CREDENTIALS</h2>
+          <form>
+            <label for="input_one" id="input_one_label">
+              FIRST NAME
+            </label>
+            <input type="text" id="input_one"></input>
+
+            <label for="input_two" id="input_two_label">
+              LAST NAME
+            </label>
+            <input type="text" id="input_two"></input>
+
+            <label for="input_three" id="input_three_label">
+              E-MAIL
+            </label>
+            <input type="text" id="input_three"></input>
+            <label for="input_four" id="input_four_label">
+              INSTITUTION
+            </label>
+            <input type="text" id="input_four"></input>
+            <input type="submit" id="submit"></input>
+          </form>
+        </div>
+      </div>
+      <div id="page_two">
+        <div id="list_one">
+          <ul>
+            <li>apurva</li>
+            <li>mishra</li>
+            <li>apurva.mishra26@yahoo.com</li>
+            <li>delhi iniversity</li>
+          </ul>
+        </div>
+        <div id="list_two">
+          <ul>
+            <li>ipsum</li>
+            <li>lorem </li>
+            <li>ipsum.lorem@yahoo.com</li>
+            <li>lucknow university</li>
+          </ul>
+        </div>
+        <div id="list_three">
+          <ul>
+            <li>some</li>
+            <li>thing </li>
+            <li>some.thing@yahoo.com</li>
+            <li>lucknow university</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 ReactDOM.render(<App />, document.getElementById("root"));
